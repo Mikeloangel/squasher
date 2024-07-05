1da1b441965d72d661d9970176ca5059492446a6
Mikhail Varushichev <mikeloangel@gmail.com>
2024-07-05 22:39:02 +0300

    comments

--- cmd/shortener/handlers/ping.go
+++ cmd/shortener/handlers/ping.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-// Ping handles a test connection to db
+// Ping handles a test connection to db sets 500 status if no connection, 200 if connection is ok
 func (h *Handler) Ping(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
